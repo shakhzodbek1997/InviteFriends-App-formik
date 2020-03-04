1644d1ea5788572d339e3e44416de3a6b46c4b13
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-03-04 18:50:14 +0500

    first commit

---
+++ src/FormTypes/InviteFriends/InviteFriends.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class InviteFriends extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default InviteFriends;
